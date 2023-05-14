c615b7838a631c2f21ee93f81f2aa8c78687fcaf
everythingiseeihate <119536308+everythingiseeihate@users.noreply.github.com>
2023-05-14 19:50:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 from Figure import *
+import turtle
 
-# Создание экземпляра класса Circle
+turtle.ht()
+
+# Создание Круга
 circle = Circle(50)
 print("Площадь круга:", circle.get_area())
 print("Длина окружности:", circle.get_perimeter())
 circle.drawing_figure("red", "orange", 1)
 
-# Создание экземпляра класса Square
-square = Square(50)
+# Создание Квадрата
+square = Square(75)
 print("Площадь квадрата:", square.get_area())
 print("Периметр квадрата:", square.get_perimeter())
 square.drawing_figure("blue", "green", 1)
 
-# Создание экземпляра класса Rectangle
+# Создание Прямоугольника
 rectangle = Rectangle(50, 100)
 print("Площадь прямоугольника:", rectangle.get_area())
 print("Периметр прямоугольника:", rectangle.get_perimeter())
 rectangle.drawing_figure("black", "yellow", 1)
 
-# Создание экземпляра класса Triangle
-triangle = Triangle(50, 60, 70)
+# Создание Треугольника
+triangle = Triangle(50, 50, 50)
 print("Площадь треугольника:", triangle.get_area())
 print("Периметр треугольника:", triangle.get_perimeter())
 triangle.drawing_figure("purple", "pink", 1)
 
-# Создание экземпляра класса Pentagon
+# Создание Пятиугольника
 pentagon = Pentagon(50)
 print("Площадь пятиугольника:", pentagon.get_area())
 print("Периметр пятиугольника:", pentagon.get_perimeter())
 pentagon.drawing_figure("brown", "grey", 1)
 
-# Создание экземпляра класса Hexagon
+# Создание Шестиугольника
 hexagon = Hexagon(50)
 print("Площадь шестиугольника:", hexagon.get_area())
 print("Периметр шестиугольника:", hexagon.get_perimeter())
 hexagon.drawing_figure("navy", "skyblue", 1)
 
-# Создание экземпляра класса Ellipse
+# Создание Эллипса
 ellipse = Ellipse(50, 100)
 print("Площадь эллипса:", ellipse.get_area())
 print("Длина периметра эллипса:", ellipse.get_perimeter())
